edc1e864de8564652cc1e194b1d0e70b966fd7bb
Thiago Durante Pires <thdurante@gmail.com>
2017-10-21 10:57:40 -0200

    Alterando a definição do modelo de AreaGeografica para utilizar es6 class

---
+++ src/models/AreaGeografica.js
@@ -0,0 +1,19 @@
+class AreaGeografica {
+
+	constructor(codigo, descricao, codigoAlternativo) {
+		Object.assign(this, {
+			codigo,
+			descricao,
+			codigoAlternativo
+		})
+	}
+
+	static areas() {
+		return ([
+			new AreaGeografica(1, 'Identificador do sujeito local',                   'L'),
+			new AreaGeografica(2, 'Identificador da área, região ou distrito',        'A'),
+			new AreaGeografica(3, 'Identificador do estado, província ou território', 'E'),
+			new AreaGeografica(4, 'Identificador nacional',                           'N'),
+		])
+	}
+}
