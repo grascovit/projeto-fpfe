99a5fa531b46215ce5fb6f858ac51b02abce99b1
Gabriel Rascovit <rascovit@gmail.com>
2017-10-21 11:44:08 -0200

    adiciona modelos para endereço e linha de endereço

---
+++ src/models/EnderecoIndividuo.js
@@ -0,0 +1,17 @@
+class EnderecoIndividuo {
+
+  constructor(linhaEndereco, bairro, municipio, codigoMunicipio, estado, 
+    codigoPostal, caixaPostal, identificadorPais, tipoEndereco) {
+      Object.assign(this, {
+        linhaEndereco, // Linha de endereço
+        bairro, // Bairro
+        municipio, // Município
+        codigoMunicipio, // Código do município
+        estado, // Estado
+        codigoPostal, // Código postal
+        caixaPostal, // Caixa postal 
+        identificadorPais, // Identificador do país
+        tipoEndereco // Tipo do endereço
+      })
+  }
+}
