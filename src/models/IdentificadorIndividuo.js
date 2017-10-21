9e90fafeeb35afdc73ad1e3e1a245561d775b01f
Thiago Durante Pires <thdurante@gmail.com>
2017-10-21 11:19:43 -0200

    Atributos adicionais do tipo do identificador.

--- src/models/IdentificadorIndividuo.js
+++ src/models/IdentificadorIndividuo.js
@@ -1,11 +1,12 @@
 class IdentificadorIndividuo {
-  constructor(identificador, areaGeografica, emissor, dataDeEmissao, tipoIdentificador) {
+  constructor(identificador, areaGeografica, emissor, dataDeEmissao, tipoIdentificador, atributosAdicionaisTipoIdentificador) {
     Object.assign(this, {
       identificador, // Número ou código atribuído a uma pessoa por uma organização. Não pode ser nulo ou vazio
       areaGeografica, // Código que representa a área geográfica na qual o identificador é utilizado
       emissor, // Organização que aloca uma designação do identificador de um indivíduo
       dataDeEmissao, // A data na qual foi emitido o documento de identificação do indivíduo. Tipo: data
-      tipoIdentificador // Código que identifica o tipo de identificador. Tipo: texto. Exemplo: carteira de trabalho e certidão de nascimento
+      tipoIdentificador, // Código que identifica o tipo de identificador. Tipo: texto. Exemplo: carteira de trabalho e certidão de nascimento
+      atributosAdicionaisTipoIdentificador // 3.1.2 Componentes opcionais - Vai ser tratado na view e salvo como atributos adicionais
     })
   }
 }
