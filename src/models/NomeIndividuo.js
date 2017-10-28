6a142cf718767d39fba39f3eacf804e04a7fba74
pauloXtr3m <paulo-gyn@outlook.com>
2017-10-28 16:17:30 -0200

    insere nome alternativo no modelo

--- src/models/NomeIndividuo.js
+++ src/models/NomeIndividuo.js
@@ -3,6 +3,7 @@ class NomeIndividuo{
     Object.assign(this, {
       titulos, //titulos do individuo, EX.: Prof., Dr.. Não obrigatório
       nome, //nome do individuo, Não obrigatório
+      nomeAlternativo,//Estabelece a representação de um nome quando a representação alfabética não é aquela usada na comunidade.
       indicadorUso, //indicador de uso opcional para nome, Indicador, Não obrigatório
       sobrenome, //Sobrenome do individuo, Obrigatório
       sufixos
